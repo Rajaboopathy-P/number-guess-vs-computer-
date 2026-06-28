d491156b9553c67983b11c665b13d8ac11ebc4ce
Rajaboopathy P <rajaboopathy825@gmail.com>
2026-06-28 22:06:36 +0530

    Refactor game.js by removing unused code
    Removed alert and event listeners for user input.

--- Game/game.js
+++ Game/game.js
@@ -1,18 +1,4 @@
 
-            alert(`Enter a number between ${min} and ${max}`);
-            return;'click', playRound);
-    document.getElementById('resetBtn').addEventListener('click', resetGame);
-    difficultySel.addEventListener('change', () => {
-        setDifficulty();
-        resetGame();
-    });
-
-    humanInput.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            playRound();
-        }
-    });
 
     setDifficulty();
     updateUI('Ready');
